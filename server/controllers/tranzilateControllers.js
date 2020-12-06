02283b0b3c3581a6446f2c354d791463f30d362e
abdullah-fahad <72708185+abdullah-fahad@users.noreply.github.com>
2020-12-07 02:24:48 +0300

    Update tranzilateControllers.js

--- server/controllers/tranzilateControllers.js
+++ server/controllers/tranzilateControllers.js
@@ -12,20 +12,30 @@ export const tran = async(req, res) => {
     }
 
     try{
-        const codeArray = code.split(' ');
-        let tranz = '';
-        for (var x = 0; x<codeArray.length; x++){
-             const find = findOne(codeArray[x]);
-            if(!find){
-             tranz = `${tranz + codeArray[x]}`
-        }  else if(find){
-            console.log(find);
-            tranz = `${tranz + source.javaScript_code[find]} `;
-        }
-        };
-        res.status(200).json({message:tranz});
+        const codeArray = code.split('');
+        let codeBuilder = "",tran = "",err1 = '', err2 = '', err3 = '', issue = `${err1 + "\n" + err2 + '\n' + err3}`;
+        for(var x = 0; x < codeArray.length; x++){
+            codeBuilder = `${codeBuilder + codeArray[x]}`;
+            switch (codeArray[x]){
+                case '{':
+                    err1 = `. يتوقع وجود "}" في انتهاء اي ميثود أو كائن`;
+                    break;
+                case '}':
+                    err1 = '';
+                    break;
+                case '(':
+                    err2 = '. يتوقع وجود ")" بعد الانتهاء من اسناد القيم للباراميترات في الدوال';
+                    break;
+                case ')':
+                    err2 = '';
+                    break;
+            }
+            const find = findOne(codeBuilder)
+            if (!find) {continue} else {tran = `${tran + source.javaScript_code[find]}`; codeBuilder = '';}
+            }  
+        res.status(200).json({message:tran, error:`${err1+err2+err3}`});
+        console.log(codeArray);
     }catch (e) {
-        res.status(500).json(e.message);
     }
 }
 
